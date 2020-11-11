db191e662add0fdc11049a86e7075c03b4aa94e9
Michael Jay Lupos <michael.lupos@appetiser.com.au>
2020-11-11 09:53:59 +0800

    added index and save function in EventController

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Event;
 
 class EventController extends Controller
 {
     public function index()
     {
         return Event::all();
+    }
+
+    public function save (Request $request) 
+    {
+        $data = $request->only(['name', 'date_to', 'date_from', 'days']);
+        $saveData = Event::create([
+            'days' => $data['days'],
+            'name' => $data['name'],
+            'date_to' => $data['date_to'],
+            'date_from' => $data['date_from']
+        ]);
+        return response()->json($saveData);
     }
 }
