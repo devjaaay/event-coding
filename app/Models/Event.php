f0e221afdc1f4ebf270d4707eb6b4ee9aa68a1c0
Michael Jay Lupos <michael.lupos@appetiser.com.au>
2020-11-11 09:53:48 +0800

    added fillable in event model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'days',
+        'date_from',
+        'date_to'
+    ];
 }
